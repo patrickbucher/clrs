3e9bf51aa03ea56f1cecd4e0a71f0b8df25ee2dc
Patrick Bucher <patrick.bucher@mailbox.org>
2026-03-10 21:43:02 +0100

    implemented insertion sort with benchmarks

--- sorting.go
+++ sorting.go
@@ -1,1 +1,18 @@
 package clrs
+
+import "cmp"
+
+func InsertionSort[T cmp.Ordered](items []T) {
+	n := len(items)
+	if n <= 1 {
+		return
+	}
+	for i := 1; i < n; i++ {
+		key := items[i]
+		j := i - 1
+		for ; j >= 0 && items[j] > key; j-- {
+			items[j+1] = items[j]
+		}
+		items[j+1] = key
+	}
+}
--- sorting_test.go
+++ sorting_test.go
@@ -1,1 +1,51 @@
 package clrs
+
+import "testing"
+
+const SmallBenchSize = 100
+const BigBenchSize = 100000
+
+func TestInsertionSort(t *testing.T) {
+	tests := [][]int{
+		{},
+		{1},
+		{1, 2},
+		{2, 1},
+		{1, 2, 3},
+		{2, 1, 3},
+		{3, 2, 1},
+		{3, 1, 2},
+		{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
+		{9, 8, 7, 6, 5, 4, 3, 2, 1, 0},
+		{5, 6, 4, 7, 3, 8, 2, 9, 1, 0},
+		{0, 5, 6, 4, 7, 3, 8, 2, 9, 1},
+		{-45, 45, 99},
+		{99, 45, -45},
+	}
+	for _, test := range tests {
+		n := len(test)
+		original := make([]int, n)
+		copy(original, test)
+		InsertionSort(test)
+		if !IsSorted(test) {
+			t.Errorf("InsertionSort(%v) sorted as %v\n", original, test)
+		}
+	}
+}
+
+func BenchmarkInsertionSortSmall(b *testing.B) {
+	benchmarkInsertionSort(b, SmallBenchSize)
+}
+
+func BenchmarkInsertionSortBig(b *testing.B) {
+	benchmarkInsertionSort(b, BigBenchSize)
+}
+
+func benchmarkInsertionSort(b *testing.B, size int) {
+	for b.Loop() {
+		b.StopTimer()
+		items := RandInts(0, size, size)
+		b.StartTimer()
+		InsertionSort(items)
+	}
+}
