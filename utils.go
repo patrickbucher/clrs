3f0918a6d4cb7de21479df13029d45b35cf128ea
Patrick Bucher <patrick.bucher@mailbox.org>
2026-03-10 21:16:16 +0100

    check sequence for being sorted

--- utils.go
+++ utils.go
@@ -1,6 +1,9 @@
 package clrs
 
-import "math/rand"
+import (
+	"cmp"
+	"math/rand"
+)
 
 func RandInts(min, max, n int) []int {
 	if min > max {
@@ -11,4 +14,17 @@ func RandInts(min, max, n int) []int {
 		ints[i] = rand.Intn(max-min+1) + min
 	}
 	return ints
+}
+
+func IsSorted[T cmp.Ordered](items []T) bool {
+	n := len(items)
+	if n <= 1 {
+		return true
+	}
+	for i := 1; i < n; i++ {
+		if cmp.Compare(items[i-1], items[i]) == 1 {
+			return false
+		}
+	}
+	return true
 }
--- utils_test.go
+++ utils_test.go
@@ -13,7 +13,31 @@ func TestRandInts(t *testing.T) {
 	}
 	for i, v := range ints {
 		if v < min || v > max {
-			t.Fatalf("ints[%d] = %d is out of range [%d,%d]\n", i, v, min, max)
+			t.Fatalf("ints[%d] = %d is out of range [%d,%d]\n",
+				i, v, min, max)
+		}
+	}
+}
+
+func TestIsSorted(t *testing.T) {
+	tests := []struct {
+		items  []int
+		sorted bool
+	}{
+		{[]int{}, true},
+		{[]int{0}, true},
+		{[]int{0, 0}, true},
+		{[]int{0, 1}, true},
+		{[]int{0, 0}, true},
+		{[]int{1, 0}, false},
+		{[]int{0, 9, 10}, true},
+		{[]int{0, 10, 9}, false},
+	}
+	for _, test := range tests {
+		actual := IsSorted(test.items)
+		if actual != test.sorted {
+			t.Errorf("expected Sorted(%v) to be %v, was %v\n",
+				test.items, test.sorted, actual)
 		}
 	}
 }
