03460bacc21d846356e7ec7340e11796ac910b1d
Patrick Bucher <patrick.bucher@mailbox.org>
2026-03-15 17:38:57 +0100

    more reliable sorting tests

--- sorting_test.go
+++ sorting_test.go
@@ -2,6 +2,7 @@ package clrs
 
 import (
 	"cmp"
+	"slices"
 	"testing"
 )
 
@@ -10,35 +11,23 @@ const BigBenchSize = 100000
 
 type InPlaceSortFunc[T cmp.Ordered] func([]T)
 
-var tests = [][]int{
-	{},
-	{1},
-	{1, 2},
-	{2, 1},
-	{1, 2, 3},
-	{2, 1, 3},
-	{3, 2, 1},
-	{3, 1, 2},
-	{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
-	{9, 8, 7, 6, 5, 4, 3, 2, 1, 0},
-	{5, 6, 4, 7, 3, 8, 2, 9, 1, 0},
-	{0, 5, 6, 4, 7, 3, 8, 2, 9, 1},
-	{-45, 45, 99},
-	{99, 45, -45},
-	{9, 1, 2, 8, 7, 3, 4, 6, 5, 0},
-	{5, 5, 6, 6, 4, 4, 7, 7, 8, 8, 3, 3, 2, 2, 1, 1, 9, 9, 0, 0},
+var tests = []struct {
+	input    []int
+	expected []int
+}{
+	{[]int{}, []int{}},
+	{[]int{1}, []int{1}},
+	{[]int{1, 2}, []int{1, 2}},
+	{[]int{2, 1}, []int{1, 2}},
+	{[]int{2, 1, 3}, []int{1, 2, 3}},
+	{[]int{4, 1, 2, 3}, []int{1, 2, 3, 4}},
+	{[]int{5, 4, 3, 2, 1}, []int{1, 2, 3, 4, 5}},
+	{[]int{1, 2, 3, 4, 5}, []int{1, 2, 3, 4, 5}},
+	{[]int{1, 1, 1, 1, 1}, []int{1, 1, 1, 1, 1}},
 }
 
 func TestInsertionSort(t *testing.T) {
-	for _, test := range tests {
-		n := len(test)
-		work := make([]int, n)
-		copy(work, test)
-		InsertionSort(work)
-		if !IsSorted(work) {
-			t.Errorf("InsertionSort(%v) sorted as %v\n", test, work)
-		}
-	}
+	testSort(t, InsertionSort)
 }
 
 func BenchmarkInsertionSortSmall(b *testing.B) {
@@ -50,15 +39,7 @@ func BenchmarkInsertionSortBig(b *testing.B) {
 }
 
 func TestSelectionSort(t *testing.T) {
-	for _, test := range tests {
-		n := len(test)
-		work := make([]int, n)
-		copy(work, test)
-		SelectionSort(work)
-		if !IsSorted(work) {
-			t.Errorf("SelectionSort(%v) sorted as %v\n", test, work)
-		}
-	}
+	testSort(t, SelectionSort)
 }
 
 func BenchmarkSelectionSortSmall(b *testing.B) {
@@ -70,23 +51,7 @@ func BenchmarkSelectionSortBig(b *testing.B) {
 }
 
 func TestMergeSort(t *testing.T) {
-	for _, test := range tests {
-		n := len(test)
-		work := make([]int, n)
-		copy(work, test)
-		MergeSort(work)
-		if !IsSorted(work) {
-			t.Errorf("SelectionSort(%v) sorted as %v\n", test, work)
-		}
-	}
-}
-
-func TestMergeSortDebug(t *testing.T) {
-	orig := []int{4, 1, 2, 3}
-	n := len(orig)
-	work := make([]int, n)
-	copy(work, orig)
-	MergeSort(work)
+	testSort(t, MergeSort)
 }
 
 func BenchmarkMergeSortSmall(b *testing.B) {
@@ -95,6 +60,18 @@ func BenchmarkMergeSortSmall(b *testing.B) {
 
 func BenchmarkMergeSortBig(b *testing.B) {
 	benchmarkInPlaceSort(b, MergeSort, BigBenchSize)
+}
+
+func testSort(t *testing.T, f InPlaceSortFunc[int]) {
+	for _, test := range tests {
+		n := len(test.input)
+		work := make([]int, n)
+		copy(work, test.input)
+		f(work)
+		if !slices.Equal(work, test.expected) {
+			t.Errorf("sorted %v as %v\n", test, work)
+		}
+	}
 }
 
 func benchmarkInPlaceSort(b *testing.B, f InPlaceSortFunc[int], size int) {
